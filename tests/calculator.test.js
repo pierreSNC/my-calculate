9ef7d58fb877b206861f6312450253c1beddecf1
pierreSNC <56020645+pierreSNC@users.noreply.github.com>
2025-03-13 09:52:20 +0100

    feat: second test -> subtracting

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,5 +1,9 @@
-const { add } = require("../src/calculator");
+const { add, subtract  } = require("../src/calculator");
 
 test("adding 2 and 3 must return 5", () => {
     expect(add(2, 3)).toBe(5);
+});
+
+test("subtracting 5 and 3 should return 2", () => {
+    expect(subtract(5, 3)).toBe(2);
 });
