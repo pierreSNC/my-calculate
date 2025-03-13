8450e129c6fb228033e161af90785bfb2152c90d
pierreSNC <56020645+pierreSNC@users.noreply.github.com>
2025-03-13 10:02:16 +0100

    feat: third test -> multiply

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,4 +1,4 @@
-const { add, subtract  } = require("../src/calculator");
+const { add, subtract, multiply   } = require("../src/calculator");
 
 test("adding 2 and 3 must return 5", () => {
     expect(add(2, 3)).toBe(5);
@@ -6,4 +6,8 @@ test("adding 2 and 3 must return 5", () => {
 
 test("subtracting 5 and 3 should return 2", () => {
     expect(subtract(5, 3)).toBe(2);
+});
+
+test("multiplication of 4 and 3 must return 12", () => {
+    expect(multiply(4, 3)).toBe(12);
 });
